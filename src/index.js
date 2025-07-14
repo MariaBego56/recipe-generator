d82a2507c8bd5299c4cb6ffb004fc193a0e2b470
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-07-14 21:45:12 +0100

    updated both index.js and index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-alert("hello world");
+function generateIngredientsList(event){
+event.preventDefault();
+
+let recipeFormElement = document.querySelector("#recipeGeneratorForm");
+recipeFormElement = addEventListener("submit", generateIngredientsList);
+
+
+
+  let input = document.querySelector(".AddYourIngredients");
+  let rawIngredients = input.value.trim();
+  if (rawIngredients.length === 0) return;
+
+
+  let ingredients = rawIngredients.split(",").map(item => item.trim()).filter(item => item !== "");
+
+  let list = document.querySelector("#ingredient-list");
+
+  ingredients.forEach((ingredient) => {
+    
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    li.appendChild(span);
+    list.appendChild(li);
+
+    new Typewriter(span, {
+      strings: ingredient,
+      autoStart: true,
+      delay: 50,
+    });
+  });
+
+  input.value = ""; 
+}
